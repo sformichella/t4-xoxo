669c5b75c0461d75db2e51101df39be9968fed59
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-20 10:39:10 -0700

    Update to results page graph colors and error message with instructions to play if no data

--- results/results.js
+++ results/results.js
@@ -1,43 +1,48 @@
 import { renderHeader } from '../header/header.js';
 import { wins, losses, ties } from '../data/data.js';
 import arrayOfRounds from '../data/data.js';
+import {getFromLocalStorage} from '../utils.js';
 
 renderHeader();
 
-const chartWins = wins;
-const chartLosses = losses;
-const chartTies = ties;
+const chartWins = getFromLocalStorage('Wins');
+const chartLosses = getFromLocalStorage('Losses');
+const chartTies = getFromLocalStorage('Ties');
 const labels = ['Wins', 'Losses', 'Ties'];
 const chartData = [chartWins, chartLosses, chartTies];
+const mainChart = document.getElementById('myChart');
+const noData = document.getElementById('no-data');
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {                        //eslint-disable-line
+let ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {         //eslint-disable-line
     type: 'pie',
     data: {
         labels: labels,
         datasets: [{
-            label: '# of Votes',
+            label: 'Count of Games',
             data: chartData,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
+                'rgba(255, 255, 0, 0.2)',
+                'rgba(255, 0, 0, 0.2)',
+                'rgba(0, 255, 255, 0.2)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
+                'rgba(255, 255, 0, 1)',
+                'rgba(255, 0, 0, 1)',
+                'rgba(0, 255, 255, 1)',
             ],
-            borderWidth: 1
+            borderWidth: 5
         }]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
         }
     }
 });
+
+//Handing no data
+if (chartLosses === null && chartWins === null && chartTies === null) {
+    //if no data, hide chart, display instructions to play game
+    mainChart.style.display = 'none';
+    noData.textContent = 'Click \'Reset\' and play some games to populate game history.';
+}
